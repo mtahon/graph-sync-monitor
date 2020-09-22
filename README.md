42668b7b4e35bf23a2aea1e8e30804ac7d1a91f1
Mathieu TAHON <mathieu.tahon@noath.net>
2020-09-22 17:33:31 +0200

    Added readme and made the speed based on average instead of instant

---
+++ README.md
@@ -0,0 +1,54 @@
+# The Graph - Syncronization Monitor
+
+This simple Python tools monitor the syncronization of a subgraph and predicts a target date for syncronization
+
+## Installation
+
+Clone the repository:
+
+```shell
+git clone git@github.com:mtahon/graph-sync-monitor.git
+```
+
+Change directory:
+
+```shell
+cd graph-sync-monitor
+```
+
+Create a virtual environment:
+
+```shell
+python3 -m venv env
+```
+
+Activate the virtual environment:
+
+```shell
+source env/bin/activate
+```
+
+Install the dependencies
+
+```shell
+pip install -r requirements.txt
+```
+
+## Running the monitoring
+
+Start the monitoring:
+
+```shell
+$ python sync.py
+Current Block: 10527783 | Speed: 0.747518 b/s | Target: 2020-09-23 13:37:37.123809
+Current Block: 10527791 | Speed: 0.747765 b/s | Target: 2020-09-23 13:37:12.830740
+Current Block: 10527797 | Speed: 0.746723 b/s | Target: 2020-09-23 13:38:56.285456
+Current Block: 10527800 | Speed: 0.744641 b/s | Target: 2020-09-23 13:42:25.478459
+Current Block: 10527802 | Speed: 0.742115 b/s | Target: 2020-09-23 13:46:40.870269
+```
+
+It displays key metrics:
+
+* Current Block being indexed
+* Indexing Speed in Block per second, as an average since the monitoring started
+* Target date to reach the requested block at the average indexing speed
--- sync.py
+++ sync.py
@@ -7,19 +7,30 @@ from datetime import datetime, timedelta
 base_url = 'https://api.thegraph.com/index-node/graphql'
 payload = {"query" : "{indexingStatusForCurrentVersion(subgraphName: \"mtahon/uniswap-tokenlist\") { chains { latestBlock { hash number }}}}"}
 target_block = 10581999
-current_block = 0
+first_block = 0
 interval = 10
 
+# Get the current block number
 def get_block_number():
     r = requests.post(base_url, json=payload)
     return int(r.json()['data']['indexingStatusForCurrentVersion']['chains'][0]['latestBlock']['number'])
 
+# Define the start parameters
+start_time = datetime.now()
+start_block = get_block_number()
+current_block = start_block
+
 while(current_block < target_block):
+    # Get the new block and time
     previous_block = current_block
+    current_time = datetime.now()
     current_block = get_block_number()
+
+    # Skip 
     if(previous_block >= current_block):
+        print("No Progress")
         continue
-    speed = (current_block - previous_block) / interval
+    speed = (current_block - start_block) / (current_time - start_time).seconds
     remaining = (target_block - current_block) / speed
     target_date = datetime.now() + timedelta(seconds=remaining)
     print("Current Block: %i | Speed: %f b/s | Target: %s" % (current_block, speed, target_date))
